8f61b75708108491d97659b808db2a3089d6b429
Sagarwithkumar <101035861+Sagarwithkumar@users.noreply.github.com>
2023-09-15 02:10:13 +0530

    Updates

---
+++ precache-manifest.cb77d1dfab65d6f62e40a3b9ddf96170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "793c769ca6654e8911bc",
+    "url": "/WordCheck-React/static/css/main.4e0ff555.chunk.css"
+  },
+  {
+    "revision": "793c769ca6654e8911bc",
+    "url": "/WordCheck-React/static/js/main.793c769c.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/WordCheck-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/WordCheck-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "51dd8d722d92c4d8c8b1",
+    "url": "/WordCheck-React/static/js/runtime~main.51dd8d72.js"
+  },
+  {
+    "revision": "f5a6aaf2e2a1a43fdf91febb985df128",
+    "url": "/WordCheck-React/index.html"
+  }
+];
